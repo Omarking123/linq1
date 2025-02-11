eaf099bff0f126ddfe6f58958be15c48cbe36b07
Omar <Omar@DESKTOP-OUKQKJG>
2025-02-11 08:47:08 +0300

    video 3

--- Program.cs
+++ Program.cs
@@ -6,21 +6,36 @@
         {
             #region video 1
             //Employee e01 = new Employee() { Id = 1, Name = "ahmed ", Salary = 1200 };
-            var e01 = new { Id = 1, Name = "ahmed ", Salary = 1200 };
+            //var e01 = new { Id = 1, Name = "ahmed ", Salary = 1200 };
 
-            Console.WriteLine(e01.Id); Console.WriteLine(e01.Name);
+            //Console.WriteLine(e01.Id); Console.WriteLine(e01.Name);
 
-            Console.WriteLine(e01.GetType().Name);
+            //Console.WriteLine(e01.GetType().Name);
 
             #endregion
 
 
             #region video 2 
-            int number = 12345;
-            var result = Intextension.Revers(number);
-            Console.WriteLine(result);
+            //int number = 12345;
+            //var result = Intextension.Revers(number);
+            //Console.WriteLine(result);
 
-            number.Revers(); 
+            //number.Revers();
+            #endregion
+
+            #region video 3 
+
+            //List<int> Numbers = new List<int>() { 1, 2, 3, 4 };
+
+            ////var result3 = Enumerable.Where(Numbers, N => N % 2 == 0);
+            ////var result3 = Enumerable.Any(Numbers, N => N % 2 == 0)
+            //var result3 = Enumerable.Range(1, 100);
+            //Console.WriteLine(result3);
+
+            //foreach (var item in result3)
+            //{
+            //    Console.WriteLine(item);
+            //} 
             #endregion
         }
     }
