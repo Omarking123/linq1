e13dc35b7c728320e4ec1272714ca8380ebd65ae
Omar <Omar@DESKTOP-OUKQKJG>
2025-02-11 08:16:35 +0300

    Video one

--- Program.cs
+++ Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            #region video 1
+            //Employee e01 = new Employee() { Id = 1, Name = "ahmed ", Salary = 1200 };
+            var e01 = new { Id = 1, Name = "ahmed ", Salary = 1200 };
+
+            Console.WriteLine(e01.Id); Console.WriteLine(e01.Name);
+
+            Console.WriteLine(e01.GetType().Name);
+
+            #endregion
         }
     }
 }
